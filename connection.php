5e90ee1110f97a52387114211cccb077756de5ae
Gourav Sharma <gourav.sharma@novoinvent.com>
2022-08-05 22:27:09 +0530

    changes

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php 
 function DbConnection(){
-    $servername = "localhost:6706";
-    $username = "root";
-    $password = "";
-    $db = "tutorial_website";
+    $servername = "localhost";
+    $username = "u657940708_user";
+    $password = "Staging123$";
+    $db = "u657940708_tutorial_web";
 
     // Create connection
     $DbConn = new mysqli($servername, $username, $password,$db);
