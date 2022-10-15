638ce028d0bdf0204e62c7191c0dad31c2480b6a
Gourav Sharma <gourav.sharma@novoinvent.com>
2022-10-15 14:49:20 +0530

    Changes

--- contact.php
+++ contact.php
@@ -32,6 +32,18 @@ while($row = $data->fetch_assoc()){
 label{
     width: 10%;
 }
+@media only screen and (max-width: 768px) {
+    label{
+        width: 25%;
+    }
+    textarea{
+        height: 15rem !important;
+        width: 65%;
+    }
+    input{
+        width: 65%;
+    }
+}
 textarea{
     height: 15rem !important;
     width: 35%;
@@ -105,7 +117,7 @@ input{
             <span class="f-twitter">
                 <section class="mb-0"><br><p><b>ApnaTutorial</b> is an online platform which is designed to learn new technologies with a highly interactive user interface.</p></section><section class=""><div class="row foot"><div class="col-lg-3 col-md-6 mb-1 mb-md-0">
                     <ul class="list-unstyled mb-0"><li><a class="foot-link" href="about.php" style="display: inherit;">About</a></li></ul></div><div class="col-lg-3 col-md-6 mb-1 mb-md-0"><ul class="list-unstyled mb-0"><li><a class="foot-link" href="privacy.php" style="display: inherit;">Privacy Policy</a></li></ul></div><div class="col-lg-3 col-md-6 mb-1 mb-md-0"><ul class="list-unstyled mb-0"><li><a class="foot-link" href="contact.php" style="display: inherit;">Contact Us</a></li></ul></div></div></section></div>
-                    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">© 2020 Copyright:<a class="text-white" href="/index.php">ApnaTutorial</a></div>
+                    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">© 2022 Copyright:<a class="text-white" href="/index.php">ApnaTutorial</a></div>
     </div>
     <script>
 function submitForm(){
--- content.php
+++ content.php
@@ -159,7 +159,7 @@ ul{
 </td></tr></tbody></table></div>
     <div class="headermobile">
     <div style="margin-top:10px;padding:0px;text-align:left;padding-top:0.5rem;">
-    <span style="float:left;padding-top:2px;"><a href="index.php" style="color:#000000;text-decoration:none;padding-left: 1.5rem;"><span style="background:#0fc6b0;padding: 3px 6px;border-radius: 50%;">A</span>pnaTutorial</a></span> 
+    <span style="float:left;padding-top:2px;"><a href="../" style="color:#000000;text-decoration:none;padding-left: 1.5rem;"><span style="background:#0fc6b0;padding: 3px 6px;border-radius: 50%;">A</span>pnaTutorial</a></span> 
     <span style="position: relative;right: 1.5rem;bottom: 2.5px;" onclick="showmenu()"><i class="fa-solid fa-bars" style="font-size: 30px;float: right;"></i></span>
 </div>
     <div style="margin:0px;padding:0px;clear:both">
